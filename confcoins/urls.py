29ad94aced1af4e787e7d914a4c05313e2785e8c
HKawahara8575 <kawahara857@gmail.com>
2022-01-23 21:36:25 +0900

    2nd1

--- confcoins/urls.py
+++ confcoins/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
-    path('checkcoins', include('checkcoins.urls')),
+    path('checkcoins/', include('checkcoins.urls')),
 ]
